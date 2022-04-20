f38c10b01e66dd20a710f70d2d1268e3527a7f14
aniketstiwari <anikaittiwari@gmail.com>
2022-04-21 00:28:55 +0530

    second approach sum two pair equal to x

--- misc/sum_two_pair.rb
+++ misc/sum_two_pair.rb
@@ -5,24 +5,24 @@
 
 # sorting two pointer technique
 
-def pair_with_sum_x(arr, sum)
-	size = arr.length
-	left = 0 
-	right = size - 1
-	arr = arr.sort
-	while left < right do
-		if arr[left] + arr[right] == sum
-			return 1
-		elsif arr[left] + arr[right] < sum
-			left += 1
-		else
-			right -= 1
-		end
-	end
-	return -1
-end
+# def pair_with_sum_x(arr, sum)
+# 	size = arr.length
+# 	left = 0 
+# 	right = size - 1
+# 	arr = arr.sort
+# 	while left < right do
+# 		if arr[left] + arr[right] == sum
+# 			return 1
+# 		elsif arr[left] + arr[right] < sum
+# 			left += 1
+# 		else
+# 			right -= 1
+# 		end
+# 	end
+# 	return -1
+# end
 
-p pair_with_sum_x([1, 4, 45, 6, 10, -8], 16)
+# p pair_with_sum_x([1, 4, 45, 6, 10, -8], 16)
 
 # Time Complexity
 
@@ -32,3 +32,22 @@ p pair_with_sum_x([1, 4, 45, 6, 10, -8], 16)
 # Auxiliary Space
 
 # This too depends on sorting algorithm. The auxiliary space is O(n) for merge sort and O(1) for Heap Sort.
+
+
+##########################################################################
+
+
+# 2nd Approach
+
+def pair_with_sum_x(arr, sum)
+	hsh = {}
+	arr.each_with_index do |elem, index|
+		temp = sum - arr[index]
+		if hsh.has_key?(temp)
+			puts "#{sum} is available #{temp} #{arr[index]}"
+		end
+		hsh[arr[index]] = index
+	end
+end
+
+pair_with_sum_x([1, 4, 45, 6, 10, -8, 12], 16)
