379e50868413a0fed1c3bd5ddc8593fbeafcb86b
aniketstiwari <anikaittiwari@gmail.com>
2022-04-30 15:47:21 +0530

    HEAp initial implementation

---
+++ heap/implementation.py
@@ -0,0 +1,29 @@
+import heapq
+import pdb
+
+arr = [5, 1, 4, 2, 7]
+
+heapq.heapify(arr)
+#print (list(arr))
+
+heapq.heappush(arr, 8)
+
+#print(list(arr))
+
+heapq.heappop(arr)
+heapq.heappop(arr)
+
+#print(list(arr))
+
+li1 = [5, 7, 9, 4, 3]
+  
+# initializing list 2
+li2 = [5, 7, 9, 4, 3]
+ 
+
+print (heapq.heappushpop(li1, 6))
+
+print(li1)
+
+
+print (heapq.heapreplace(li2, 2))
